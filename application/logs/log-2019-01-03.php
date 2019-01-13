1ee27251dd6b96b53ae17ebe9f3bc935d429e352
htousi1 <h-tounsi1@live.fr>
2019-01-13 16:19:13 +0100

    all project

---
+++ application/logs/log-2019-01-03.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-01-03 00:04:24 --> Query error: La table 'lms.ci_sessions' n'existe pas - Invalid query: SELECT `data`
+FROM `ci_sessions`
+WHERE `id` = 'i7kbji9amfqt650v83sdns4715pan9c5'
+ERROR - 2019-01-03 00:09:32 --> Severity: Warning --> require_once(C:\wamp64\www\conges-management\vendor/autoload.php): failed to open stream: No such file or directory C:\wamp64\www\conges-management\application\controllers\Connection.php 12
+ERROR - 2019-01-03 00:09:32 --> Severity: Compile Error --> require_once(): Failed opening required 'C:\wamp64\www\conges-management\vendor/autoload.php' (include_path='.;C:\php\pear') C:\wamp64\www\conges-management\application\controllers\Connection.php 12
---
+++ application/logs/log-2019-01-10.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-01-10 13:45:53 --> {controllers/session/login} Invalid login id or password for user=bbalet
+ERROR - 2019-01-10 13:46:02 --> {controllers/session/login} Invalid login id or password for user=bbalet
